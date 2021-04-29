4fc8b05ad25cac2eb2d0c8dfe15ebe8d90c33290
HAZEL HENG <20029318@myrp.edu.sg>
2021-04-29 16:46:24 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/ep/c346/id20029318/l02resgisterationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.ep.c346.id20029318.l02resgisterationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
